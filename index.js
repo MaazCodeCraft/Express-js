574eb4a47991f0c3ef8f437cdc5f6a8c7bf8b1eb
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-15 19:51:29 +0500

    Built-In Middleware

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-
+app.use(express.urlencoded( { etended: true } )); // key=value&key=value
+app.use(express.static('public'));
 
 app.use(logger);
 
