b8f297e6c5111daded652cd5c5af6381d7833231
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-17 19:45:54 +0500

    Callback Hell

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,18 +1,33 @@
+// Asynchronous
 console.log('Before');
-const user = getUser(1);
-console.log(user);
+getUser(1, (user) => {
+    getRepositories(user.gitHubUserName, (repos) => {
+        getcommits(repo, (commits) => {
+            // CALLBACK HELL
+        });
+    });
+});
 console.log('Afer');
 
+// Synchronous
+console.log('Before');
+const user = getUser(1);
+const repos = getRepositories(user.gitHubUserName);
+const commits = getCommits(repos[0]);
+console.log('Afer');
 
 // Callbacks
-// Promises
-// Async/await
 
-function getUser (id) {
+function getUser (id, callback) {
     setTimeout(() => {
         console.log('Reading a user from a database....');
-        return { id: id, gitHubUserName: 'maaz' };
+        callback({ id: id, gitHubUserName: 'maaz' });
     }, 2000);
+}
 
-    return 1;
+function getRepositories (username, callback) {
+    setTimeout(() => {
+        console.log('Calling gitHub API...');
+        callback(['repo1', 'repo2', 'repo3']);
+    }, 2000);
 }
