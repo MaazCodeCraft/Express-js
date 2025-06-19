75670c8d98b51013fbab19e2b5441b9fd5d4c1f5
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-19 16:21:26 +0500

    Async and Await

--- async-demo/replacing-callbacks-to-promises.js
+++ async-demo/replacing-callbacks-to-promises.js
@@ -1,4 +1,5 @@
 console.log('Before');
+// Callback based-approach
 // getUser(1, (user) => {
 //     getRepositories(user.gitHubUsername, (repos) => {
 //         getCommits(repos[0], (commits) => {
@@ -7,12 +8,25 @@ console.log('Before');
 //     })
 // });
 
-getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits: ', commits))
-    .catch(err => console.log('Error: ', err));
+// Promise based-approach
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits: ', commits))
+//     .catch(err => console.log('Error: ', err));
 
+// Async and Await based-approach
+async function displayCommits () {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    } catch (err) {
+        console.log('Error ', err.message);
+    }
+}
+displayCommits();
 console.log('After');
 
 function getUser(id) {
@@ -28,7 +42,8 @@ function getRepositories(username) {
     return new Promise ((resolve, reject) => {
         setTimeout(() => {
             console.log('Calling GitHub API...');
-            resolve(['repo1', 'repo2', 'repo3']);
+            // resolve(['repo1', 'repo2', 'repo3']);
+            reject(new Error ('could not get a repos..'));
         }, 2000);
     });
 }
