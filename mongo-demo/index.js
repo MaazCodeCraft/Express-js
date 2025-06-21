a1dba8feb71adc629e952386a4c75f3726a5663f
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-21 21:48:59 +0500

    Schemas

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1/playground')
     .then(() => console.log('Connected to mongoDB'))
     .catch(err => console.error('Could not connect to mongoDB', err));
+
+const courseSchema = mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
