7316ed82bc80d700ca410819a984c0009d51b5b8
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-22 22:29:55 +0500

    Logical Query Operators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -27,6 +27,7 @@ async function createCourse() {
 }
 
 async function getCourses () {
+    // Comparision
     // eq(equal)
     // ne(not equal)
     // gt(greater than)
@@ -36,10 +37,17 @@ async function getCourses () {
     // in
     // nin (not in)
 
+    // Logical Opreators
+    // or
+    // and
+
     const courses = await Course
-    // .find({ author: 'Maaz', isPublished: true })'
+    // .find({ author: 'Maaz', isPublished: true })
     // .find({ price: { $gte: 10, $lte: 20 } })
-    .find({ price: { $in: [10, 15, 20] } })
+    // .find({ price: { $in: [10, 15, 20] } })
+    .find()
+    .or([ { author: 'Maaz' }, { isPublished: true } ])
+    .and([])
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 })
