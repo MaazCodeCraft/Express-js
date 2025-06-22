fc5e0d00209ffdffb54533ed2daf32014147859e
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-22 22:36:33 +0500

    Regular Expressions

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -43,11 +43,25 @@ async function getCourses () {
 
     const courses = await Course
     // .find({ author: 'Maaz', isPublished: true })
+
     // .find({ price: { $gte: 10, $lte: 20 } })
     // .find({ price: { $in: [10, 15, 20] } })
-    .find()
-    .or([ { author: 'Maaz' }, { isPublished: true } ])
-    .and([])
+
+    // .find()
+    // .or([ { author: 'Maaz' }, { isPublished: true } ])
+    // .and([])
+
+    
+    // Starts with Maa
+    .find({ author: /^Maa/ })
+
+    // Ends with Rahman
+    .find({author: /Rahman$/i}) // i for case insensitive 
+
+    // Contain Maaz
+    .find({ author: /.*Maaz.*/ })
+
+
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 })
