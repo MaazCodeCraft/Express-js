f807e14a31200f89921c314c4a42a56c746646ce
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-23 16:29:05 +0500

    Exercise 3

---
+++ mongo-demo/exercise/solution-3.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://127.0.0.1/mongo-exercise')
+    .then(() => console.log('Connected to mongoDB'))
+    .catch((err) => console.log('Could not connect to mongoDB'));
+
+const courseScheme = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+    Price: Number
+});
+
+const Course = mongoose.model('Course', courseScheme);
+
+async function getCourses() {
+    return await Course
+        .find({ isPublished: true })
+        .or([
+            { price: { $gte: 15 } },
+            { name: /.*by.*/i }
+        ])
+        .sort('-price')
+        .select('name author price');
+}
+
+async function run() {
+    const courses = await getCourses();
+    console.log(courses);
+}
+
+run();
