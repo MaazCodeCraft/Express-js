d6b1a02cab9041bdac598e7d83ae5d441ecdcd1e
Maaz Ur Rahman <maaz4cs@gmail.com>
2025-06-26 12:54:40 +0500

    Custom Validators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,3 +1,4 @@
+const { validate } = require('joi/lib/types/alternatives');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://127.0.0.1/playground')
@@ -18,7 +19,15 @@ const courseSchema = new mongoose.Schema({
         enum: ['web', 'mobile', 'network']
     },
     author: String,
-    tags: [ String ],
+    tags: {
+        type: Array,
+        validate: {
+            validator: function(v) {
+                return v && v.length > 0;
+            },
+            message: 'A course should at leat one tag.'
+        }
+    },
     date: { type: Date, default: Date.now },
     isPublished: Boolean,
     price: { 
@@ -36,9 +45,9 @@ const Course = mongoose.model('Course', courseSchema);
 async function createCourse() {
     const course = new Course ({
         name: "Node Course",
-        category: "-",
+        category: "web",
         author: "Maaz",
-        tags: ['Node', 'Backend'],
+        tags: [],
         isPublished: true,
         price: 15
     });
